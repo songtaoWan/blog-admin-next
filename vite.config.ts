bff6bbc3c7b42f94ca4868a0cf172aeca1577e62
wansongtao <18101837209@163.com>
2023-06-02 11:39:44 +0800

    chore: 移除按需引入ant-design插件，最新版的ant-design不需要该插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import Components from 'unplugin-vue-components/vite'
-import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    Components({
-      resolvers: [
-        AntDesignVueResolver({
-          importStyle: false // css in js
-        })
-      ]
-    })
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -28,5 +16,5 @@ export default defineConfig({
     open: true,
     host: '0.0.0.0'
   },
-  base: '/admin/',
+  base: '/admin/'
 })
