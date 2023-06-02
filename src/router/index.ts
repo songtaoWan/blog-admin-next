a3654ac2cbe413a1e1a03351f96d68e67644ec59
wansongtao <18101837209@163.com>
2023-06-02 09:32:49 +0800

    fix: 修复路由404错误

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+import NotFound from '@/views/NotFound.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
       name: 'login',
-      component: () => import('@/views/LoginView.vue')
+      component: LoginView
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
-      component: () => import('@/views/notFound.vue')
+      component: NotFound
     }
   ]
 })
