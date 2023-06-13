6b1b986347666102649e203684841d88c46b4ae7
wansongtao <18101837209@163.com>
2023-06-13 21:34:01 +0800

    feat: 网站全屏组件封装

---
+++ src/components/ScreenFull/ScreenFull.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import {
+  FullscreenExitOutlined,
+  FullscreenOutlined
+} from '@ant-design/icons-vue'
+import { useFullscreen } from '@vueuse/core'
+
+const { isFullscreen, toggle } = useFullscreen()
+</script>
+
+<template>
+  <div class="screen_full px-5">
+    <fullscreen-exit-outlined v-show="isFullscreen" @click="toggle" />
+    <fullscreen-outlined v-show="!isFullscreen" @click="toggle" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.screen_full {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  color: var(--bg-top-menuColor);
+}
+</style>
