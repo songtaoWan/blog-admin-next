d34663f5b5a8718b05d738e2c50b81fb7b7c5436
wansongtao <18101837209@163.com>
2023-06-13 21:22:29 +0800

    feat: 收缩/展开侧边菜单栏组件封装

---
+++ src/components/CollapsedButton/CollapsedButton.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined
+} from '@ant-design/icons-vue'
+import { useSettingStore } from '@/stores/setting'
+import { storeToRefs } from 'pinia'
+
+const setStore = useSettingStore()
+const { toggleCollapsed } = setStore
+const { collapsed } = storeToRefs(setStore)
+</script>
+
+<template>
+  <div class="collapsed px-5">
+    <menu-fold-outlined v-show="!collapsed" @click="toggleCollapsed" />
+    <menu-unfold-outlined v-show="collapsed" @click="toggleCollapsed" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.collapsed {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 16px;
+  color: var(--bg-top-menuColor);
+}
+</style>
