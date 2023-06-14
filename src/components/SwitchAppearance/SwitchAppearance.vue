44c683e4b5ca2eea21f9bf7a142d2a06eeb20835
wansongtao <18101837209@163.com>
2023-06-14 22:08:38 +0800

    feat: 网站皮肤切换组件封装

---
+++ src/components/SwitchAppearance/SwitchAppearance.vue
@@ -0,0 +1,74 @@
+<script lang="ts" setup>
+import IconSun from '../icons/IconSun.vue'
+import IconMoon from '../icons/IconMoon.vue'
+import { useSettingStore } from '@/stores/setting'
+import { storeToRefs } from 'pinia'
+
+const setStore = useSettingStore()
+const { toggleTheme } = setStore
+const { theme } = storeToRefs(setStore)
+</script>
+
+<template>
+  <button class="switch-appearence">
+    <span
+      class="switch-check"
+      :class="theme === 'dark' ? 'switch-check--active' : ''"
+      @click="toggleTheme"
+    >
+      <span class="switch-icon">
+        <IconSun v-show="theme === 'light'" />
+        <IconMoon v-show="theme === 'dark'" />
+      </span>
+    </span>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.switch-appearence {
+  position: relative;
+  border-radius: 11px;
+  display: block;
+  width: 40px;
+  height: 22px;
+  flex-shrink: 0;
+  border: 1px solid var(--vt-c-divider);
+  background-color: var(--vt-c-bg-mute);
+  transition: border-color 0.25s, background-color 0.25s;
+
+  &:hover {
+    border-color: var(--vt-c-gray);
+  }
+
+  .switch-check {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 1px;
+    left: 1px;
+    width: 18px;
+    height: 18px;
+    color: var(--vt-c-text-2);
+    border-radius: 50%;
+    background-color: var(--vt-c-white);
+    box-shadow: var(--vt-shadow-1);
+    transition: all 0.25s;
+  }
+
+  .switch-check--active {
+    transform: translateX(18px);
+    color: var(--vt-c-text-1);
+    background-color: var(--vt-c-black);
+  }
+
+  .vt-switch-icon {
+    position: relative;
+    display: block;
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+    overflow: hidden;
+  }
+}
+</style>
