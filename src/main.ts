5764ef7867166d9ee2640b428e4782aba7e74325
wansongtao <18101837209@163.com>
2023-06-17 12:28:15 +0800

    feat: 初始化主题

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import '@/assets/styles/base.css'
 import '@/assets/styles/reset-menu.css'
 import 'tailwindcss/tailwind.css'
+import { initTheme } from './utils/theme'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+initTheme()
 const app = createApp(App)
 
 app.use(createPinia())
