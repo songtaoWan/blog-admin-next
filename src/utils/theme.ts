5f99f63ac93a2123804eea2a9408e41400095b31
wansongtao <18101837209@163.com>
2023-06-18 20:26:19 +0800

    feat: 改用web-storage-plus库使用web存储

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,13 +1,13 @@
 import type { ITheme } from '@/types/index'
 import { defaultTheme } from '@/config/index'
-import { getStorage, setStorage } from '@/utils/storage'
+import { getStorage, setStorage } from 'web-storage-plus'
 
 const key = 'theme'
 
 export const setTheme = (theme: ITheme) => {
   document.body.className = theme
 
-  setStorage({ key, value: theme })
+  setStorage(key, theme)
 }
 
 export const getTheme = () => {
