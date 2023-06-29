3228c0e82679037177a79ff502c5e0668ee51add
wansongtao <18101837209@163.com>
2023-06-29 14:39:53 +0800

    feat: 添加静态路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,63 @@ import LoginView from '@/views/LoginView.vue'
 import NotFound from '@/views/NotFound.vue'
 import LayoutView from '@/layout/LayoutView.vue'
 
+export const adminRoute = {
+  path: '/',
+  component: LayoutView,
+  meta: {
+    title: '首页'
+  },
+  children: [
+    {
+      path: '',
+      name: 'home',
+      component: () => import('@/views/home/HomeView.vue'),
+      meta: {
+        title: '首页',
+        icon: 'PieChartOutlined'
+      }
+    },
+    {
+      path: 'userinfo',
+      name: 'UserInfo',
+      component: () => import('@/views/userinfo/UserInfo.vue'),
+      meta: {
+        title: '个人中心',
+        hidden: true
+      }
+    },
+    {
+      path: 'article',
+      name: 'Article',
+      component: () => import('@/components/ParentView/ParentView.vue'),
+      meta: {
+        title: '文章管理',
+        icon: 'MailOutlined'
+      },
+      children: [
+        {
+          path: 'list',
+          name: 'ArticleList',
+          component: () => import('@/views/article/list/ArticleList.vue'),
+          meta: {
+            title: '文章列表',
+            icon: 'DesktopOutlined'
+          }
+        },
+        {
+          path: 'add',
+          name: 'ArticleAdd',
+          component: () => import('@/views/article/add/ArticleAdd.vue'),
+          meta: {
+            title: '添加文章',
+            icon: 'InboxOutlined'
+          }
+        }
+      ]
+    }
+  ]
+}
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -11,37 +68,29 @@ const router = createRouter({
       name: 'Login',
       component: LoginView
     },
-    {
-      path: '/',
-      component: LayoutView,
-      meta: {
-        title: '首页'
-      },
-      children: [
-        {
-          path: '',
-          name: 'home',
-          component: () => import('@/views/home/HomeView.vue'),
-          meta: {
-            title: '首页'
-          }
-        },
-        {
-          path: 'userinfo',
-          name: 'UserInfo',
-          component: () => import('@/views/userinfo/UserInfo.vue'),
-          meta: {
-            title: '个人中心'
-          }
-        }
-      ]
-    },
+    adminRoute,
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
       component: NotFound
     }
-  ]
+  ],
+  scrollBehavior(to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if (to.hash) {
+      return {
+        el: to.hash
+      }
+    }
+
+    return {
+      top: 0,
+      behavior: 'smooth'
+    }
+  }
 })
 
 export default router
