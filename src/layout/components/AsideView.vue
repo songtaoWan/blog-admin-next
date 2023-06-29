5897e0744950ab51a1653f9c535981bac890db3b
wansongtao <18101837209@163.com>
2023-06-29 14:40:35 +0800

    feat: 根据静态路由生成侧边菜单栏

--- src/layout/components/AsideView.vue
+++ src/layout/components/AsideView.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { Menu as AMenu } from 'ant-design-vue'
-import { reactive, watch, h } from 'vue'
+import { reactive, watch, h, type VNode } from 'vue'
 import {
   PieChartOutlined,
   MailOutlined,
@@ -10,84 +10,67 @@ import {
 } from '@ant-design/icons-vue'
 import { useSettingStore } from '@/stores/setting'
 import { storeToRefs } from 'pinia'
+import { adminRoute } from '@/router/index'
+import type { RouteRecordRaw } from 'vue-router'
+import type { MenuInfo } from 'ant-design-vue/es/menu/src/interface'
+import { useRouter } from 'vue-router'
+
+interface MenuItem {
+  key: string
+  icon?: () => VNode
+  label?: string
+  path: string
+  children?: MenuItem[]
+}
+
+const iconComponents: Record<string, any> = {
+  PieChartOutlined,
+  MailOutlined,
+  DesktopOutlined,
+  InboxOutlined,
+  AppstoreOutlined
+}
+const getMenus = (routers: RouteRecordRaw[], parentPath = '/'): MenuItem[] => {
+  const menus: MenuItem[] = []
+
+  routers.forEach((item) => {
+    if (item.meta?.hidden) {
+      return
+    }
+
+    const { meta, name } = item
+    let path = parentPath
+    if (item.redirect) {
+      path = item.redirect as string
+    } else if (item.path) {
+      path = path[path.length - 1] !== '/' ? path + '/' + item.path : path + item.path
+    }
+
+    const menuItem: MenuItem = {
+      key: name as string,
+      icon: meta?.icon ? () => h(iconComponents[meta.icon!]) : undefined,
+      label: meta?.title,
+      path
+    }
+
+    if (item.children && item.children.length > 0) {
+      menuItem.children = getMenus(item.children, path)
+    }
+
+    menus.push(menuItem)
+  })
+
+  return menus
+}
 
 const setStore = useSettingStore()
 const { collapsed } = storeToRefs(setStore)
-
 const state = reactive<{ selectedKeys: string[]; openKeys: string[]; preOpenKeys: string[] }>({
-  selectedKeys: ['1'],
+  selectedKeys: [],
   openKeys: [],
   preOpenKeys: []
 })
-const items = reactive([
-  {
-    key: '1',
-    icon: () => h(PieChartOutlined),
-    label: 'Option 1'
-  },
-  {
-    key: '2',
-    icon: () => h(DesktopOutlined),
-    label: 'Option 2'
-  },
-  {
-    key: '3',
-    icon: () => h(InboxOutlined),
-    label: 'Option 3'
-  },
-  {
-    key: 'sub1',
-    icon: () => h(MailOutlined),
-    label: 'Navigation One',
-    children: [
-      {
-        key: '5',
-        label: 'Option 5'
-      },
-      {
-        key: '6',
-        label: 'Option 6'
-      },
-      {
-        key: '7',
-        label: 'Option 7'
-      },
-      {
-        key: '8',
-        label: 'Option 8'
-      }
-    ]
-  },
-  {
-    key: 'sub2',
-    icon: () => h(AppstoreOutlined),
-    label: 'Navigation Two',
-    children: [
-      {
-        key: '9',
-        label: 'Option 9'
-      },
-      {
-        key: '10',
-        label: 'Option 10'
-      },
-      {
-        key: 'sub3',
-        label: 'Submenu',
-        children: [
-          {
-            key: '11',
-            label: 'Option 11'
-          },
-          {
-            key: '12',
-            label: 'Option 12'
-          }
-        ]
-      }
-    ]
-  }
-])
+
 watch(
   () => state.openKeys,
   (_val, oldVal) => {
@@ -100,6 +83,24 @@ watch(
     state.openKeys = val ? [] : state.preOpenKeys
   }
 )
+
+const items = reactive<MenuItem[]>(getMenus(adminRoute.children!, adminRoute.path))
+
+const router = useRouter()
+const onChangeRoute = ({ item }: MenuInfo) => {
+  router.push(item.path)
+}
+
+watch(
+  () => router.currentRoute.value.name,
+  (val) => {
+    if (state.selectedKeys.includes(val as string)) {
+      return
+    }
+
+    state.selectedKeys = [val as string]
+  }
+)
 </script>
 
 <template>
@@ -110,6 +111,7 @@ watch(
       mode="inline"
       :inline-collapsed="collapsed"
       :items="items"
+      @click="onChangeRoute"
     ></a-menu>
   </aside>
 </template>
